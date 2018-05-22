b2138de43c166c93ca3cfb8ea8b4112bd91b618e
Alcino Fujii <alcino.fujii@campinas.sottelli.com.br>
2018-05-22 17:20:41 -0300

    Initial commit

---
+++ feature/src/main/java/trufas/kj/feature/MainActivity.kt
@@ -0,0 +1,18 @@
+package trufas.kj.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+    }
+
+    external fun stringFromJNI(): String
+
+}
